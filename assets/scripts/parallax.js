c53f3b6d8e3f9c20a87086c72ac31e4286944a15
martin <66722680+t-ub3@users.noreply.github.com>
2023-09-02 16:25:52 +0100

    a

--- assets/scripts/parallax.js
+++ assets/scripts/parallax.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const parallaxSections = document.querySelectorAll(".banner");
 
     window.addEventListener("scroll", function () {
-        parallaxSections.forEach(function (section, index) {
-            const scrollPosition = window.scrollY;
-            section.style.backgroundPositionY = -(scrollPosition * 0.5) + "px"; // Adjust the factor for desired parallax effect
+        if (window.innerWidth > 842)
+            parallaxSections.forEach(function (section, index) {
+                const scrollPosition = window.scrollY;
+                section.style.backgroundPositionY = -(scrollPosition * 0.5) + "px"; // Adjust the factor for desired parallax effect
+            });
         });
     });
-});
